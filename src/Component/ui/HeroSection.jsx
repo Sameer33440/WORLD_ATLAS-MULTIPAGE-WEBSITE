8ed3e73fbdef1cb1d95b777730540a0e0b008519
Sameer33440 <sameersinha1310@gmail.com>
2025-02-09 12:39:35 +0530

    Update HeroSection.jsx

--- src/Component/ui/HeroSection.jsx
+++ src/Component/ui/HeroSection.jsx
@@ -9,7 +9,7 @@ export const HeroSection = () => {
                 <button className="btn btn-darken btn-inline bg-white-box">
                     Start Exploring<FaLongArrowAltRight /> </button> </div>
             <div className="hero-image">
-                <img src="./public/Images/world.png" alt=" " className="banner-image" />
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVD5NTnwcQU5V9GzgZHb6YV6pqNGecVjphqw&s" alt=" " className="banner-image" />
             </div>
         </div>
     </main>)
